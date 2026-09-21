074171e338a1a62e7baf925b28b5ddbaaf6462ab
bengu <bengu@MacBook-Pro.local>
2026-09-22 01:37:23 +0200

    run.py guncellendi

---
+++ run.py
@@ -0,0 +1,14 @@
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.get("/")
+def home():
+    return "NUIT AND CO çalışıyor."
+
+if __name__ == "__main__":
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000)),
+    )
